de4cfc5ec62582c59d7618cb95412580c44a7356
ihsan maulana <ihsan.maulana@paper.id>
2022-09-15 09:07:37 +0700

    rm node module

--- dist/ticket-open/ticket-open.service.d.ts
+++
@@ -1,6 +0,0 @@
-import { TicketOpenGateway } from "./ticket-open.gateway";
-export declare class TicketOpenService {
-    private ticketOpenGateway;
-    constructor(ticketOpenGateway: TicketOpenGateway);
-    pushTicketOpen(data: any): Promise<any>;
-}
